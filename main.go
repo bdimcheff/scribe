dd095f60d68e5967495f1d5dc56f7307c8c87043
Brandon Dimcheff <bdimchef-git@wieldim.com>
2015-10-14 15:00:33 -0400

    silly test things hardcoded everywhere

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	//"log"
+	"log"
 	"os"
 
-	//"github.com/olark/scribe/version"
-)
+	syslog "github.com/olark/scribe/syslog"
 
-import flag "github.com/spf13/pflag"
+	//"github.com/olark/scribe/version"
+	flag "github.com/spf13/pflag"
+)
 
 var server string
 var reprintLogs bool
@@ -16,6 +17,16 @@ func main() {
 	flag.StringVarP(&server, "server", "s", "localhost", "syslog server to log to")
 	flag.BoolVarP(&reprintLogs, "print", "p", true, "reprint log lines to stdout for further capture")
 	flag.Parse()
+
+	logger, err := syslog.Dial("tcp", "localhost:10514", syslog.LOG_DEBUG, "brandon-test")
+
+	if err != nil {
+		log.Println("Error connecting to syslog, exiting")
+		log.Println(err)
+		os.Exit(1)
+	}
+
+	logger.Info("shouldbeinfo ||| {\"conversation_id\": \"abc123\"}")
 
 	os.Exit(0)
 }
