b66be02e2ec69ffe3191c24a991684470cf4cb4b
Jeff Zellner <jeffz@eml.cc>
2016-03-02 11:09:50 -0700

    more moving

--- scribe/scribe.go
+++ scribe/scribe.go
@@ -103,22 +103,3 @@ func Run(opts *Options) {
 		}
 	}
 }
-
-func logRaw(level string, message ...interface{}) {
-	now := time.Now()
-	timestamp := now.Format("2006-01-02 15:04:05.000")
-
-	fmt.Printf("%s - %s - scribe - %s\n", timestamp, level, message)
-}
-
-func logMessage(message ...interface{}) {
-	logRaw("INFO", message)
-}
-
-func logError(message ...interface{}) {
-	logRaw("ERROR", message)
-}
-
-func logDebug(message ...interface{}) {
-	logRaw("DEBUG", message)
-}
